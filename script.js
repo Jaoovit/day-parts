526e9205553801d92524fd1b198585f61977429a
João Oliveira <jaoovit@gmail.com>
2023-10-23 11:02:47 +0100

    create a way to insert the image and the background color for each part of the day

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+function charge (){
+
+let changeString = window.document.getElementById("h")
+let img = window.document.getElementById("image")
 let now = new Date()
 let dayHours = now.getHours()
 
-    if (dayHours <= 12){
-    console.log('Morning')
-} else if(dayHours <=18){
-    console.log ('Afternoon')
+changeString.innerHTML += `It's ${now} hours now`;
+    if (dayHours >= 0 && dayHours < 12){
+    img.src = 'images/morning.jpg'
+    document.body.style.background = '#e2cd9f'
+} else if(dayHours >= 12 && dayHours <18){
+    img.src = 'images/afternoon.jpg'
+    document.body.style.background = '#b9846f'
 } else {
-    console.log ('Night')
+    img.src = 'images/night.jpg'
+    document.body.style.background = '#515154'
 }
+
+}
+
